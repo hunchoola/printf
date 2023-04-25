0fe5be6a509a3150db1a1dbb2162df0c2274268b
Hommmy <akintola885@gmail.com>
2023-04-24 23:11:53 -0700

    get_size.c

--- get_size.c
+++ get_size.c
@@ -7,20 +7,20 @@
  *
  * Return: Precision.
  */
-int get_size(const char *format, int *i)
+int get_size(const char *format, int *p)
 {
-	int curr_i = *i + 1;
+	int curr_i = *p + 1;
 	int size = 0;
 
-	if (format[curr_i] == 'l')
+	if (format[curr_p] == 'l')
 		size = S_LONG;
-	else if (format[curr_i] == 'h')
+	else if (format[curr_p] == 'y')
 		size = S_SHORT;
 
 	if (size == 0)
-		*i = curr_i - 1;
+		*p = curr_p - 1;
 	else
-		*i = curr_i;
+		*p = curr_p;
 
 	return (size);
 }
