708a5cfc857795cda6c1b668d4dd9701e087a8df
Hommmy <akintola885@gmail.com>
2023-04-25 20:57:50 -0700

    utils.c

--- utils.c
+++ utils.c
@@ -6,9 +6,9 @@
  *
  * Return: 1 if c is printable, 0 otherwise
  */
-int is_printable(char c)
+int is_printable(char v)
 {
-	if (c >= 32 && c < 127)
+	if (v >= 32 && v < 127)
 		return (1);
 
 	return (0);
@@ -21,18 +21,18 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int append_hexa_code(char ascii_code, char buffer[], int i)
+int append_hexa_code(char ascii_code, char buffer[], int v)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	buffer[i++] = '\\';
-	buffer[i++] = 'x';
+	buffer[v++] = '\\';
+	buffer[v++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[v++] = map_to[ascii_code / 16];
+	buffer[v] = map_to[ascii_code % 16];
 
 	return (3);
 }
